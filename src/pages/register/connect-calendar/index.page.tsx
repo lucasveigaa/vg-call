3233b900c66ad47c4512e0f918f74ca0097e276b
Lucas Veiga <lucasveigagonzaga@yahoo.com.br>
2023-02-20 18:21:50 -0300

    create connect calendar page

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,37 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+
+import { Container, Header } from '../styles'
+import { ConnectBox, ConnectItem } from './styles'
+import { RegisterFormData } from './types'
+
+export default function Register() {
+  // async function handleRegister(data: RegisterFormData) {}
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao VG Call!</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button variant="secondary" size="sm">
+            Conectar <ArrowRight />
+          </Button>
+        </ConnectItem>
+
+        <Button type="submit">
+          Próximo passo <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
